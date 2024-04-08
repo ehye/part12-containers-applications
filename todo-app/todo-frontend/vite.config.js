75cbdeeb7a36648b74d2467d69a8582ca3c9220a
ehye <yjw9610@hotmail.com>
2024-04-08 19:41:22 +0800

    12.15

--- todo-app/todo-frontend/vite.config.js
+++ todo-app/todo-frontend/vite.config.js
@@ -3,6 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    hmr: {
+      host: 'localhost',
+    },
+    watch: {
+      usePolling: true,
+    },
+  },
   plugins: [react()],
   test: {
     environment: 'jsdom',
